e0d1841595b634a648ebe84ec5f51cd2a849ca8d
cherifb16 <cherifbobbyray@gmail.com>
2019-12-02 09:24:49 +0000

    validation

--- spec/models/user_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  # describe '#email' do
-  #   it { should_not allow_value("blah").for(:email) }
-  #   it { should allow_value("a@b.com").for(:email) }
-  # end
-end
