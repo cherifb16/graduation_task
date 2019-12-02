8e873387805264e47f87e5a607320e311c4b9a99
cherifb16 <cherifbobbyray@gmail.com>
2019-12-02 09:17:42 +0000

    back here

--- spec/models/car.spec.rb
+++ spec/models/car.spec.rb
@@ -1,23 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  
-  background do
-    @car = Car.new(licensePlateNumber: "9F4J1LN",
-                    manufacturer: "HERO HONDA MOTORS LTD",
-                    model: "model5",
-                    hourlyRentalRate: 240.4,
-                    style: "SUV",
-                    location: "67948-9956",
-                    status: "Available")
-  
-  end
-  test "car should be suggested" do
-    assert_not @car.isSuggested?
-  end
-  
-  test "car should be available" do
-    assert @car.isAvailable?
-  end
+ it "Validation  pass if none of the attribute is empty" do
+   
+  @car = Car.new(licensePlateNumber: "9F4J1LN",
+  manufacturer: "HERO HONDA MOTORS LTD",
+  model: "model5",
+  hourlyRentalRate: 240.4,
+  style: "SUV",
+  location: "67948-9956",
+  status: "Available")
+
+   expect(@car).to be_valid
+ end
+ it "Validation does not pass if manufacturer is empty" do
+  @car = Car.new(licensePlateNumber: "r",
+  manufacturer: "",
+  model: "model5",
+  hourlyRentalRate: 240.4,
+  style: "SUV",
+  location: "67948-9956",
+  status: "Available")
+   expect(@car).not_to be_valid
+ end
+ it "validation goes not pass If location is empty" do
+  @car = Car.new(licensePlateNumber: "",
+  manufacturer: "",
+  model: "model5",
+  hourlyRentalRate: 240.4,
+  style: "SUV",
+  location: "",
+  status: "Available")
+   expect(@car).not_to be_valid
+ end
  
 end
