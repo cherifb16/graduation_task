a38776c46b38ad27058c6d78ac540556807d86fe
cherifb16 <cherifbobbyray@gmail.com>
2019-12-03 09:49:09 +0000

    css+

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
   config.serve_static_assets = true
   config.assets.compile = true
   config.assets.digest = true
